de83c088d266bb4c543dbc3e0f681b8de252cf0d
Mothkuri Prince <130035014+princemothkuri@users.noreply.github.com>
2024-08-19 17:02:44 +0530

    Update GroupChatModal.js

--- src/components/miscellaneous/GroupChatModal.js
+++ src/components/miscellaneous/GroupChatModal.js
@@ -58,7 +58,7 @@ const GroupChatModal = ({ children }) => {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      const { data } = await axios.get(`https://chat-app-server-r2l6.onrender.com/api/user?search=${search}`, config);
       console.log(data);
       setLoading(false);
       setSearchResult(data);
@@ -96,8 +96,7 @@ const GroupChatModal = ({ children }) => {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.post(
-        `/api/chat/group`,
+      const { data } = await axios.post(`https://chat-app-server-r2l6.onrender.com/api/chat/group`,
         {
           name: groupChatName,
           users: JSON.stringify(selectedUsers.map((u) => u._id)),
