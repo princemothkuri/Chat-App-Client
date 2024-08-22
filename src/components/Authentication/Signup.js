6076183d6a56fdd6b155efc6ec6054612b6913e8
Mothkuri Prince <130035014+princemothkuri@users.noreply.github.com>
2024-08-22 21:03:16 +0530

    Update Signup.js

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -51,7 +51,7 @@ const Signup = () => {
         },
       };
       const { data } = await axios.post(
-        "/api/user",
+        "https://chat-app-server-r2l6.onrender.com/api/user",
         {
           name,
           email,
